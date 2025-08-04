d029955313f0a7ba48e17ad41176890cdf8722f9
ctph <curwentph@gmail.com>
2025-08-04 11:35:32 -0500

    fix reload not found error

--- src/App.js
+++ src/App.js
@@ -1,19 +1,21 @@
 import React from "react";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import HomePage from "./frontend/layout/HomePage";
 import Well96 from "./frontend/layout/96well";
 import Well384 from "./frontend/layout/384well";
-import SmartChipPage from "./frontend/layout/Smartchip";  
-
-const router = createBrowserRouter([
-  { path: "/", element: <HomePage /> },
-  { path: "/96well", element: <Well96 /> },
-  { path: "/384well", element: <Well384 /> },
-  { path: "/Smartchip", element: <SmartChipPage /> },  
-]);
+import SmartChipPage from "./frontend/layout/Smartchip";
 
 function App() {
-  return <RouterProvider router={router} />;
+  return (
+    <HashRouter>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/96well" element={<Well96 />} />
+        <Route path="/384well" element={<Well384 />} />
+        <Route path="/Smartchip" element={<SmartChipPage />} />
+      </Routes>
+    </HashRouter>
+  );
 }
 
 export default App;
